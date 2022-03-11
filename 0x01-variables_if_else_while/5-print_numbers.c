617619b90af9a5582cba148f1ecf92b3dec17a5e
Hunegna Demissie Bekele <92586974+Hunegn@users.noreply.github.com>
2022-03-11 09:10:36 -1200

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,5 +1,15 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: Hunegna Demissie
+ */
+
 #include <stdio.h>
 
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int num;
