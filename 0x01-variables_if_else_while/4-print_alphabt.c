5e8b8857cea7abedc8b59a7e8f23c6a4b57fafb2
Hunegna Demissie Bekele <92586974+Hunegn@users.noreply.github.com>
2022-03-11 09:12:34 -1200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,15 @@
+/*
+ * File: 4-print_alphabt.c
+ * Auth: Hunegna Demissie
+ */
+
 #include <stdio.h>
 
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	char letter;
