20250006ff0834f0c3b205f3082f042a82b58880
Hunegna Demissie Bekele <92586974+Hunegn@users.noreply.github.com>
2022-03-11 09:47:13 -1200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,17 +6,23 @@
  */
 int main(void)
 {
-int k;
+int x, y;
 
-for (k = 48; k <= 57; k++)
+<<<<<<< HEAD
+for (m = 97; m <= 122; m++)
+putchar(m);
+for (n = 65; n <= 90; n++)
+putchar(n);
+=======
+for (x = 97; x<= 122; x++)
 {
-putchar(k);
-if (k < 57)
+putchar(x);
+}
+for (y = 65; y <= 90; y++)
 {
-putchar(',');
-putchar(' ');
+putchar(y);
 }
-}
+>>>>>>> c38d008b18676f16774155151031a0fcc1c46752
 putchar('\n');
 return (0);
 }
